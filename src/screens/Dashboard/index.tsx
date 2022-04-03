ba4ca3454dc419a9c7cd28d9b577356193563b67
Miranda <omirandavictor@gmail.com>
2022-04-02 21:11:05 -0300

    Starting to create a Header Component

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,10 +1,32 @@
 import React from 'react';
 
-import { Container, Title } from './styles'
+import {
+  Container,
+  Header,
+  UserWrapper,
+  UserInfo,
+  Photo,
+  User,
+  UserGreeting,
+  Username,
+  Icon
+} from './styles'
+
 export function Dashboard(){
   return (
     <Container>
-      <Title>Dashboard</Title>
+      <Header>
+        <UserWrapper>
+          <UserInfo>
+            <Photo source={{ uri: 'https://media-assets.wired.it/photos/615d95dfd8b66b13086cf465/16:9/w_1280,c_limit/Tony-Stark.png'}}/>
+            <User>
+              <UserGreeting>Olá,</UserGreeting>
+              <Username>Miranda</Username>
+            </User>
+          </UserInfo>
+          <Icon name="power"/>
+        </UserWrapper>
+      </Header>
     </Container>
   )
 }
