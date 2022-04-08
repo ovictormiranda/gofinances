8e402067991a7e747f67cdfd0435864fd7615149
Miranda <omirandavictor@gmail.com>
2022-04-08 07:04:27 -0300

    list transactions items is - DOING

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import  AsyncStorage from "@react-native-async-storage/async-storage";
 
 import { HighlightCard } from '../../components/HighlightCard';
 import { TransactionCard, TransactionCardProps } from '../../components/TransactionCard';
@@ -25,50 +26,42 @@ export interface DataListProps extends TransactionCardProps {
 }
 
 export function Dashboard(){
-  const data: DataListProps[] = [{
-    id: '1',
-    type: 'positive',
-    title:"Desenvolvimento de Site",
-    amount:"R$ 12.000,00",
-    category: {
-      name: 'Vendas',
-      icon: 'dollar-sign'
-    },
-    date:"13/02/2022"
-  },
-  {
-    id: '2',
-    type: 'negative',
-    title:"Beckmans Burguer",
-    amount:"R$ 59,00",
-    category: {
-      name: 'Alimentação',
-      icon: 'coffee'
-    },
-    date:"10/02/2022"
-  },
-  {
-    id: '3',
-    type: 'negative',
-    title:"Aluguel do Apartamento",
-    amount:"R$ 1.200,00",
-    category: {
-      name: 'Casa',
-      icon: 'home'
-    },
-    date:"05/02/2022"
-  },
-  {
-    id: '4',
-    type: 'negative',
-    title:"Yezzy Boost 350",
-    amount:"R$ 1.200,00",
-    category: {
-      name: 'Vendas',
-      icon: 'shopping-bag'
-    },
-    date:"13/02/2022"
-  }];
+  const [data, setData] = useState<DataListProps[]>([]);
+
+  async function loadTransactions(){
+    const dataKey = '@gofinances:transactions';
+    const response = await AsyncStorage.getItem(dataKey);
+    const transactions = response ? JSON.parse(response) : [];
+
+    const transactionsFormatted: DataListProps[] = transactions
+    .map((item: DataListProps) => {
+
+      const amount = Number(item.amount)
+      .toLocaleString('pt-BR', {
+        style: 'currency',
+        currency: 'BRL'
+      });
+
+      const date = Intl.DateTimeFormat('pt-BR', {
+        day: '2-digit',
+        month: '2-digit',
+        year: '2-digit'
+      }).format(new Date(item.date));
+
+      return {
+        id: item.id,
+        title: item.title,
+        amount,
+        type: item.type,
+        category: item.category,
+        date,
+      }
+    });
+  }
+
+  useEffect(() => {
+    loadTransactions();
+  },[])
 
   return (
     <Container>
