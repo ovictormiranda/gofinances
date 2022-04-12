767795d5fc5089581a4abcd3a8f86cbb4d1e7b89
Miranda <omirandavictor@gmail.com>
2022-04-12 07:00:24 -0300

    Login Screen is- DONE!

--- App.tsx
+++ App.tsx
@@ -20,6 +20,9 @@ import { NavigationContainer } from '@react-navigation/native';
 import { AppRoutes } from './src/routes/app.routes';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
+import { SignIn } from './src/screens/SignIn';
+
+import { AuthProvider } from './src/hooks/auth';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -37,7 +40,11 @@ export default function App() {
       <ThemeProvider theme={theme}>
         <NavigationContainer>
           <StatusBar  barStyle="light-content"/>
-          <AppRoutes />
+
+          <AuthProvider>
+            <SignIn />
+          </AuthProvider>
+
         </NavigationContainer>
       </ThemeProvider>
     </GestureHandlerRootView>
---
+++ src/hooks/auth.tsx
@@ -0,0 +1,40 @@
+import React, { createContext, ReactNode, useContext } from "react";
+
+interface AuthProviderProps {
+  children: ReactNode;
+}
+
+interface User {
+  id: string;
+  name: string;
+  email: string;
+  photo?: string;
+}
+
+
+interface IAuthContextData {
+  user: User;
+}
+
+export const AuthContext = createContext({} as IAuthContextData);
+
+function AuthProvider({ children }: AuthProviderProps){
+  const user = {
+    id: '123456',
+    name: 'Miranda',
+    email: 'miranda@email.com',
+  }
+  return(
+    <AuthContext.Provider value={{ user }}>
+      { children }
+    </AuthContext.Provider>
+  );
+}
+
+function useAuth() {
+  const context = useContext(AuthContext);
+
+  return context;
+}
+
+export { AuthProvider, useAuth }
